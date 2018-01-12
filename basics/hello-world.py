3a86382bf326a51ea942539501a6da93edc34e69
Wechuli <wechulipaul@yahoo.com>
2018-01-12 16:13:44 +0300

    first edit

---
+++ basics/hello-world.py
@@ -0,0 +1,8 @@
+anoth="Seems you got VS studio to work"
+msg = "Hello World"
+print(msg)
+print(anoth)
+a=input("Please supply me with a number") #this is a comment, just like the bash shell
+print("The number you entered is",a)
+
+
---
+++ basics/hello_name.py
@@ -0,0 +1,9 @@
+u_name =input("Please Enter Your name below:\n ")
+o_name= input("While you are at it, please also give me your second name")
+print("Hi",u_name)
+
+temp=input("Please enter the temprature in degrees:\n")
+temp=float(temp)
+faren=32+((9/5)*temp)
+
+print("Your name is",o_name,"and the temprature is",faren, "farenheight")
