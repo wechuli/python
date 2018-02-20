984d2a0fe8314073bb4143e771638654adf11289
Wechuli <wechulipaul@yahoo.com>
2018-02-20 10:06:28 +0300

    additions to classes

---
+++ classes_and_objects/many-instances.py
@@ -0,0 +1,16 @@
+class PartyAnimal:
+    x=0
+    name=''
+    def __init__(self,nam):
+        self.name=nam
+        print(self.name,'constructed')
+    def party(self):
+        self.x=self.x+1
+        print(self.name,"Party count",self.x)
+
+s = PartyAnimal('Sally')
+j = PartyAnimal('Jim')
+s.party()
+j.party()
+s.party()
+    
