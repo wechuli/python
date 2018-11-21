036bc2feecd532bd247550be3c6be31fec34f996
wechuli <wechulipaul@yahoo.com>
2018-11-21 17:33:23 +0300

    refresher

---
+++ refresher/basics/datatypes.py
@@ -0,0 +1,10 @@
+a={"a":23,"b":56,"45":"78"}
+
+b=[34,"rtrt",233]
+print(a,b)
+
+#variables are like containers.
+#store some data, pull it out later
+#They can hold all sorts of things
+#a variable in python is like a variable in mathematics: it is a named sysmnol that holds a value
+
---
+++ refresher/basics/simple.py
@@ -0,0 +1,29 @@
+a = 45
+b = 3
+print(type(a))  # this is a comment
+
+
+def add(a, b):
+    return a+b
+
+
+def division(a, b):
+    return a/b
+
+
+def power(a, b):
+    return a**b
+
+
+print(add(3, 4))
+print(division(3, 4))
+print(pow(3, 2))
+print(power(4, 2))
+
+
+def indiv(a, b):
+
+    return a//b
+
+
+print(indiv(6, 4))
