cd0619ea07f53415bb6a2e028aeab991eeb1b98d
wechuli <wechulipaul@yahoo.com>
2018-11-21 19:11:36 +0300

    data types

--- refresher/basics/datatypes.py
+++ refresher/basics/datatypes.py
@@ -1,10 +1,30 @@
-a={"a":23,"b":56,"45":"78"}
+a = {"a": 23, "b": 56, "45": "78"}
 
-b=[34,"rtrt",233]
-print(a,b)
+b = [34, "rtrt", 233, [34, 56]]
+print(a, b)
 
-#variables are like containers.
-#store some data, pull it out later
-#They can hold all sorts of things
-#a variable in python is like a variable in mathematics: it is a named sysmnol that holds a value
+# variables are like containers.
+# store some data, pull it out later
+# They can hold all sorts of things
+# a variable in python is like a variable in mathematics: it is a named sysmnol that holds a value
 
+print(a['a'])
+print(b[3][1])
+
+al, at, once = 5, 10, 23  # we can assign variables like this
+
+a = 45
+b = a
+
+print('a:', a)
+print('b:', b)
+
+a = a+1
+
+print('new a: ', a)
+print('new b: ', b)
+
+cash = 19867324678987.99
+
+eachR = cash/5
+print(eachR)
