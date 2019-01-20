96d5c3a3ccf7965891a247d843c33a0a96b3a386
Wechuli <wechulipaul@yahoo.com>
2019-01-20 16:21:07 +0300

    truthiness

---
+++ refresher/basics/logicalops.py
@@ -0,0 +1,6 @@
+day = input('Please enter a day of the week: ')
+
+if day == 'Monday' or day == 'Tuesday' or day == 'Wednesday':
+    print('Its a weekday, go to work')
+else:
+    print('Its the weekend, yeah')
