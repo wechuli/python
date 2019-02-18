35485453ad05179092b8938e763b514d93d2e0a1
wechuli <wechulipaul@yahoo.com>
2019-02-18 17:00:49 +0300

    lists basics

---
+++ refresher/lists/basic.py
@@ -0,0 +1,12 @@
+tasks = ['install python', 'learn python', 'take a break']
+
+#lenghth on list
+print(len(tasks))
+
+girls = ['Ashley','Matt','Micheal','June','July']
+girls.sort()
+print(girls[2])
+print(girls[-1])
+
+
+print('Paul' in girls)
---
+++ refresher/lists/basic2.py
@@ -0,0 +1,15 @@
+# DON'T TOUCH THIS PLEASE!
+people = ["Hanna","Louisa","Claudia", "Angela","Geoffrey", "aparna"]
+print(people)
+# DON'T TOUCH THIS PLEASE!
+
+#Change "Hanna" to "Hannah"
+people[0] = 'Hannah'
+
+#Change "Geoffrey" to "Jeffrey"
+people[-2] = 'Jeffrey'
+
+#Change "aparna" to "Aparna" (capitalize it)
+people[-1] = people[-1].capitalize()
+
+print(people)
