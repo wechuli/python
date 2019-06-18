a2e83dfb7550a3ff8b846b9a6302b500b88dcde2
Wechuli <wechulipaul@yahoo.com>
2019-06-18 18:07:47 +0300

    polymorphism

---
+++ refresher/object-oriented-python/polymorphism/basic.py
@@ -0,0 +1,4 @@
+# Polymorphism is a key principle in OOP which is the idea that an object can take on many(poly) forms(morph).
+
+# 1. Thesame class method works  in a similar way for different classes - A common implementation of this is to have a method in a base (or parent) class that is overriden by a subclass. This is called method overriding, each subclass will have a different implementation of the method
+# 2. The same operation works for different kinds of objects
