557401242075a12e71fc6953acdc52a6f3a61246
Wechuli <wechulipaul@yahoo.com>
2019-07-18 18:55:49 +0300

    using with_statement to open and close files

---
+++ file-io/with_statement.py
@@ -0,0 +1,6 @@
+with open('poem.txt') as file:
+    data = file.read()
+
+
+print(data)
+print(file.read())
---
+++ file-io/writing.py
@@ -0,0 +1,4 @@
+with open('confess.txt', 'w') as file:
+    file.write("Oh, how I desire to see you \n")
+    file.write("My eyes want to behold you \n")
+    file.write("Put me out of my misery, oh beautiful stranger")
