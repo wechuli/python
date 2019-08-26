80535d30484f2212d29035438a4933eb745bcf3c
Wechuli <wechulipaul@yahoo.com>
2019-08-26 21:04:42 +0300

    using css selectors for access and accessing data

---
+++ web-scraping/accessing_data.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>First HTML Page</title>
+</head>
+<body>
+  <div id="first" class="special">
+    <h3 data-example="yes">hi</h3>
+    <p>more text.</p>
+  </div>
+  <ol>
+    <li class="special super-special">This list item is special.</li>
+    <li class="special">This list item is also special.</li>
+    <li>This list item is not special.</li>
+  </ol>
+  <div data-example="yes">bye</div>
+</body>
+</html>
+"""
+
+soup = BeautifulSoup(html, "html.parser")
+
+el = soup.select(".special")
+# print(el)
+for item in el:
+    print(item.get_text())
+    print(item.name)
+    print(item.attrs)
--- web-scraping/notes.md
+++ web-scraping/notes.md
@@ -4,3 +4,16 @@
     - Using find - returns one matching tag
     - Using find_all - returns a list of matching tags
     - We can use CSS selectors to navigate
+
+## Navigating with CSS Selectors
+
+`select` - returns a list of elements matching a CSS selector:
+    - Select by id of foo: #foo
+    - Select by class of bar: .bar
+    - Select children: div > p
+    - Select descendents: div p
+
+## Accessing Data in Elements
+- `get_text` - access the inner text in an element
+- `name` - tag name
+- `attrs`-dictionary attributes
